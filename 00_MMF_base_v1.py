f22cbcbb0b59a0a58bd72944a2f8000ba955bfe8
Ryan Bateman <38599225+RebatemanYT@users.noreply.github.com>
2022-05-25 23:12:22 +0000

    More progress on 00_MMF_base_v1.py comments

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -4,4 +4,20 @@
 
 #********** MAIN ROUTINE **********
 
-#Set up dictionaries
+#Set up dictionaries / Lists needed to hold data
+
+#Ask user if they have used program before & show instructions if needed
+
+#Loop to get info:
+
+  #Get name (not blank)
+  #Get age (between 12 and 130)
+  #Calculate ticket price
+  #Loop to ask for snacks
+  #Calculate snack price
+  #Ask for payment method
+    #If needed, add surcharge.
+
+#Calculate total sales and profit
+
+#Output data to text file
