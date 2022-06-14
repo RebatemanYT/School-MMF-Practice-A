f020c8baf48fe61c332aff19bb8bbbc1ec018997
Ryan Bateman <38599225+RebatemanYT@users.noreply.github.com>
2022-06-14 03:10:31 +0000

    progress on ticket price

---
+++ Old Individual Testing Files/ticket_price_04_v1_old.py
@@ -0,0 +1,17 @@
+#functions go here
+
+profit = 0 #starting profit
+
+#main routine goes here
+
+  #START OF TEMP STUFF - DON'T COPY INTO MMF_base_00_v1
+name = ""
+while name != "xxx":
+  name = input("Name: ") #Name input
+  if name == "xxx":
+    break
+
+  age = int(input("Age: "))
+    #END OF TEMP STUFF - DON'T COPY INTO MMF_base_00_v1
+  
+  if age ==
