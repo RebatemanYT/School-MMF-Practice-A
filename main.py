f9ae90ace0bd77092872a44a9e991020a12370f9
Ryan Bateman <38599225+RebatemanYT@users.noreply.github.com>
2022-06-21 02:49:11 +0000

    Did MMF_base_00_v2, replaced count with ticket_count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #If line below is commented out, testing something else right now.
-#from MMF_base_00_v1 import *
+from MMF_base_00_v2 import *
 
 #If line 2 is commented out, line below shouldn't be. If line 2 isn't commented out, comment out line 5.
-from ticket_price_04_v1 import *
+#from ticket_price_04_v1 import *
